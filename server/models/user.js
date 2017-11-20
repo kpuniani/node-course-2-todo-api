6d6ee3ac289e0a341cd8b8b7a5c62e82c13e7fe9
Karan Puniani <puniani.karan@gmail.com>
2017-11-19 20:59:11 -0800

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -48,6 +48,30 @@ return user.save().then(()=>{
   return token;
 });
 };
+
+UserSchema.statics.findByToken= function(token){
+var User =this;
+var decoded;
+
+try{
+
+  decoded= jwt.verify(token,'abc123')
+}catch(err){
+
+return Promise.reject();
+}
+
+return User.findOne({
+'_id':decoded._id,
+'tokens.token':token,
+'tokens.access':'auth'
+
+})
+
+
+};
+
+
 var User=mongoose.model('User',UserSchema);
 
 
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose}=require('./db/mongoose');
 var {Todo} =require('./models/todo');
 var {User} =require('./models/user');
-
+var {authenticate}= require('./middleware/authenticate');
 var app=express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
@@ -114,6 +114,10 @@ app.post('/users',(req,res)=>{
   });
 });
 
+
+app.get('/users/me',authenticate,(req, res)=>{
+res.send(req.user);
+});
 module.exports={
   app
 };
